19cb56d0b8a66e071e7a680a74027dbeab9c2b42
Jayasree Boya <jayasree_b@yahoo.com>
2018-01-29 15:17:48 -0600

    first commit

---
+++ MVCEventCalendar/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCEventCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            bool status;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.FirstOrDefault(a => a.EventID == e.EventID);
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventId)
+        {
+            var status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Events.FirstOrDefault(a => a.EventID == eventId);
+                if (v == null) return new JsonResult {Data = new {status = false}};
+                dc.Events.Remove(v);
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
